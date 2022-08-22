5716af159862c6dfbb3b1b86b51a2945e7024fef
SamCress96 <111606778+SamCress96@users.noreply.github.com>
2022-08-22 18:53:17 -0500

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,20 @@
+# Bonus Samantha Cress Fruit Sales
+print ('Enter apple, orange, banana for max sales')
+print ('Enter cherry, blackberry, or pineapple, for min sales')
+orange = max(100, 150, 200)
+apple = max(150, 250, 200)
+banana = max(100, 50, 75)
+
+maximum = apple
+
+print ('Most Sales', maximum)
+
+cherry = min(50, 70, 40)
+blackberry = min(25, 60, 80)
+pineapple = min(10, 6, 28)
+
+minimum = pineapple
+
+print ('Least Sales', minimum)
+
+list = orange, apple, banana, cherry, blackberry, pineapple
